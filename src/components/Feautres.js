4b6d7fb3d4a0fee47ed12fea19ddc582525413d9
Zeus3hd <dafuq.ahd@gmail.com>
2019-09-28 20:51:23 +0300

    add icons to features component

--- src/components/Feautres.js
+++ src/components/Feautres.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import AvTimerIcon from "@material-ui/icons/AvTimer";
-
+import ApartmentIcon from "@material-ui/icons/Apartment";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
 const Features = () => {
   return (
     <Grid
@@ -15,6 +16,10 @@ const Features = () => {
         letterSpacing: "1px"
       }}
     >
+      <Grid item xs={12}>
+        <h2>-WHY US-</h2>
+        <hr style={{ width: "30%", opacity: 0.5 }} />
+      </Grid>
       <Grid item xs={12} sm={3}>
         <AvTimerIcon style={{ fontSize: "5rem", marginBottom: "1.5rem" }} />
         <h2>Title Here</h2>
@@ -25,7 +30,7 @@ const Features = () => {
         </p>
       </Grid>
       <Grid item xs={12} sm={3}>
-        <AvTimerIcon style={{ fontSize: "5rem", marginBottom: "2rem" }} />
+        <ApartmentIcon style={{ fontSize: "5rem", marginBottom: "2rem" }} />
         <h2>Title Here</h2>
         <p style={{ padding: "15px" }}>
           If you aren’t satisfied with the build tool and configuration choices,
@@ -34,13 +39,18 @@ const Features = () => {
         </p>
       </Grid>
       <Grid item xs={12} sm={3}>
-        <AvTimerIcon style={{ fontSize: "5rem", marginBottom: "2rem" }} />
+        <MonetizationOnIcon
+          style={{ fontSize: "5rem", marginBottom: "2rem" }}
+        />
         <h2>Title Here</h2>
         <p style={{ padding: "15px" }}>
           If you aren’t satisfied with the build tool and configuration choices,
           you can eject at any time. This command will remove the single build
           dependency from your project.
         </p>
+      </Grid>
+      <Grid item xs={12}>
+        <hr style={{ width: "50%", opacity: 0.5 }} />
       </Grid>
     </Grid>
   );
