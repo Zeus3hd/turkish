0f121477b3807d88f228e1bd9c653dacdd16441c
Zeus3hd <dafuq.ahd@gmail.com>
2019-09-28 20:13:29 +0300

    add icons, title and paragraphs to features section

--- src/components/Feautres.js
+++ src/components/Feautres.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import AvTimerIcon from "@material-ui/icons/AvTimer";
 
 const Features = () => {
   return (
@@ -7,16 +8,39 @@ const Features = () => {
       container
       spacing={3}
       justify="center"
-      style={{ textAlign: "center" }}
+      style={{
+        textAlign: "center",
+        color: "#555555",
+        lineHeight: "1.5rem",
+        letterSpacing: "1px"
+      }}
     >
       <Grid item xs={12} sm={3}>
-        <h1>Test</h1>
+        <AvTimerIcon style={{ fontSize: "5rem", marginBottom: "1.5rem" }} />
+        <h2>Title Here</h2>
+        <p style={{ padding: "15px" }}>
+          If you aren’t satisfied with the build tool and configuration choices,
+          you can eject at any time. This command will remove the single build
+          dependency from your project.
+        </p>
       </Grid>
       <Grid item xs={12} sm={3}>
-        <h1>Test</h1>
+        <AvTimerIcon style={{ fontSize: "5rem", marginBottom: "2rem" }} />
+        <h2>Title Here</h2>
+        <p style={{ padding: "15px" }}>
+          If you aren’t satisfied with the build tool and configuration choices,
+          you can eject at any time. This command will remove the single build
+          dependency from your project.
+        </p>
       </Grid>
       <Grid item xs={12} sm={3}>
-        <h1>Test</h1>
+        <AvTimerIcon style={{ fontSize: "5rem", marginBottom: "2rem" }} />
+        <h2>Title Here</h2>
+        <p style={{ padding: "15px" }}>
+          If you aren’t satisfied with the build tool and configuration choices,
+          you can eject at any time. This command will remove the single build
+          dependency from your project.
+        </p>
       </Grid>
     </Grid>
   );
