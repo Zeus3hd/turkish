d6feca796f3f0c521dfc06e29a4d7c189154df3e
Zeus3hd <dafuq.ahd@gmail.com>
2019-10-02 09:35:24 +0300

    populate about modal

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
 import Slide from "@material-ui/core/Slide";
+import modalImage from "../../assets/footer2(2).png";
+import styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -28,12 +27,19 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
+const ModalHeader = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+
 const AboutUs = props => {
   const classes = useStyles();
   return (
-    <div>
+    <div style={{ zIndex: 200, position: "absolute" }}>
       <Dialog
         fullScreen
+        scroll="paper"
         open={props.open}
         onClose={props.handleClose}
         TransitionComponent={Transition}
@@ -56,18 +62,87 @@ const AboutUs = props => {
             </Button>
           </Toolbar>
         </AppBar>
-        <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
-          </ListItem>
-        </List>
+        <ModalHeader>
+          <img src={modalImage} alt="modal-header" />
+          <div
+            style={{
+              textAlign: "center",
+              color: "#555555",
+              letterSpacing: "1px",
+              lineHeight: "2rem",
+              fontSize: "1.2rem"
+            }}
+          >
+            <h2>WHAT WE DO:</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse rutrum quam quis condimentum gravida. Cras euismod
+              consequat tempor.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexWrap: "wrap"
+              }}
+            >
+              <iframe
+                title="video one"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/sypHODsqOm4"
+                frameBorder="0"
+                allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{ margin: "1rem" }}
+              ></iframe>
+              <iframe
+                title="video two"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/sypHODsqOm4"
+                frameBorder="0"
+                allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                style={{ margin: "1rem" }}
+              ></iframe>
+            </div>
+          </div>
+          <div
+            style={{
+              textAlign: "center",
+              color: "#555555",
+              letterSpacing: "1px",
+              lineHeight: "2rem",
+              fontSize: "1.2rem"
+            }}
+          >
+            <h2>OUR CAPABILITIES:</h2>
+            <Grid
+              container
+              spacing={3}
+              justify="space-evenly"
+              style={{ width: "100%", textAlign: "center" }}
+            >
+              <Grid item>
+                <p>Lorem ipsum dolor sit </p>
+                <p>Lorem ipsum dolor sit </p>
+                <p>Lorem ipsum dolor sit </p>
+              </Grid>
+              <Grid item>
+                <p>Lorem ipsum dolor sit </p>
+                <p>Lorem ipsum dolor sit </p>
+                <p>Lorem ipsum dolor sit </p>
+              </Grid>
+              <Grid item>
+                <p>Lorem ipsum dolor sit </p>
+                <p>Lorem ipsum dolor sit </p>
+                <p>Lorem ipsum dolor sit </p>
+              </Grid>
+            </Grid>
+          </div>
+        </ModalHeader>
       </Dialog>
     </div>
   );
