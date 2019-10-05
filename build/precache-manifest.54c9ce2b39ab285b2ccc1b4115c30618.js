f1bdc9e80d1052dc7e759af096f894f317122c2d
Zeus3hd <dafuq.ahd@gmail.com>
2019-10-05 20:14:10 +0300

    build folder

---
+++ build/precache-manifest.54c9ce2b39ab285b2ccc1b4115c30618.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5449aa1b3d0a0e2b8bf191d5b1b321d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43a0e3a37efd8199ca93",
+    "url": "/static/css/main.bbecde6e.chunk.css"
+  },
+  {
+    "revision": "d015ebd20d14f52d531e",
+    "url": "/static/js/2.b2a61954.chunk.js"
+  },
+  {
+    "revision": "43a0e3a37efd8199ca93",
+    "url": "/static/js/main.2742f038.chunk.js"
+  },
+  {
+    "revision": "a1c64afc28489f52d603",
+    "url": "/static/js/runtime-main.e8a2ff1f.js"
+  },
+  {
+    "revision": "7b5a1e02880690d44db5a68c7bf3f16f",
+    "url": "/static/media/Yunus.7b5a1e02.jpg"
+  },
+  {
+    "revision": "7ef5ab023d9861a73ca52186fef042d6",
+    "url": "/static/media/abdelkareem.7ef5ab02.jpg"
+  },
+  {
+    "revision": "67fe54bf800fe7b2816523fbf1259dd5",
+    "url": "/static/media/abudana.67fe54bf.jpg"
+  },
+  {
+    "revision": "63567cd6235a874588c6ee3b2ce74eb2",
+    "url": "/static/media/airbnb.63567cd6.png"
+  },
+  {
+    "revision": "1823aa0e12c2f67017f72a4cf86170c3",
+    "url": "/static/media/header11.1823aa0e.jpg"
+  },
+  {
+    "revision": "db459c80ed0d275ff6a00239485dcae3",
+    "url": "/static/media/header2.db459c80.jpg"
+  },
+  {
+    "revision": "0df231e5291ca3d6dff3c05dd8a14fdf",
+    "url": "/static/media/headerr1.0df231e5.jpeg"
+  },
+  {
+    "revision": "fec99ff5b601c9d03a4d424e4f702eec",
+    "url": "/static/media/headerr2.fec99ff5.jpeg"
+  },
+  {
+    "revision": "38c3790f45c965b0d709feca6181ddb6",
+    "url": "/static/media/headerr3.38c3790f.jpeg"
+  },
+  {
+    "revision": "e103503f69c1820540d4d48b311e40be",
+    "url": "/static/media/logo.e103503f.png"
+  },
+  {
+    "revision": "d979df1b122c6dc1d500fd9dfa6a4421",
+    "url": "/static/media/turkishairlines.d979df1b.png"
+  },
+  {
+    "revision": "3dd232f2bb8069e293d7f4bb7c98c4c1",
+    "url": "/static/media/vodafone.3dd232f2.png"
+  }
+]);
